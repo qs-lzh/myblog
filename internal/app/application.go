76336e260d4af67c95b16a91176607d6fbfa516b
ZiHao Liu <145265650+qs-liuzh@users.noreply.github.com>
2025-09-05 09:29:00 +0800

    parse templates in ./ui/html/partials/

--- internal/app/application.go
+++ internal/app/application.go
@@ -28,14 +28,32 @@ type Data struct {
 }
 
 func (app *Application) render(w http.ResponseWriter, r *http.Request, page string, data any) {
+	// 解析base.tmpl和partials/中的模版
 	templateSet, err := template.ParseFiles("./ui/html/base.tmpl")
 	if err != nil {
 		app.ErrorHandler.ServerError(w, err, "Not found ./ui/html/base.tmpl")
 		return
 	}
+	dir := "./ui/html/partials/"
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		app.ErrorHandler.ServerError(w, err, fmt.Sprintf("Not found director %s", dir))
+		return
+	}
 
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			filePath := dir + entry.Name()
+			fmt.Println(filePath)
+			templateSet, err = templateSet.ParseFiles(filePath)
+			if err != nil {
+				app.ErrorHandler.ServerError(w, err, fmt.Sprintf("failed to parse %s", filePath))
+			}
+		}
+	}
+
+	// 解析pages/中的页面模版
 	filePath := filepath.Join("./ui/html/pages", page+".tmpl")
-
 	if _, err := os.Stat(filePath); err != nil {
 		if os.IsNotExist(err) {
 			app.ErrorHandler.ServerError(w, err, fmt.Sprintf("Not found %s", filePath))
@@ -44,13 +62,13 @@ func (app *Application) render(w http.ResponseWriter, r *http.Request, page stri
 		app.ErrorHandler.ServerError(w, err, "os.Stat(filePath) failed")
 		return
 	}
-
 	templateSet, err = templateSet.ParseFiles(filePath)
 	if err != nil {
 		app.ErrorHandler.ServerError(w, err, "failed to parse files to")
 		return
 	}
 
+	// 执行模版
 	err = templateSet.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.ErrorHandler.ServerError(w, err, "failed to execute template")
